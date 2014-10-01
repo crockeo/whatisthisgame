d02ad9f83daf5057ccae670cdd7db3d7c69b60d0
Cerek Hillen <crockeo@gmail.com>
2014-10-01 17:26:52 -0400

    Laid some groundwork for implementing animations.

---
+++ src/WhatIsThisGame/Animation.hs
@@ -0,0 +1,55 @@
+-- | This module provides some tools to run through an @'Animation'@ in the
+--   scope of FRP and Haskell.
+module WhatIsThisGame.Animation (stepArray) where
+
+--------------------
+-- Global Imports --
+import Control.Applicative
+import Control.Monad.Fix
+import FRP.Elerea.Param
+
+----------
+-- Code --
+
+-- | Making sure the index is bound by the size of the list.
+bound :: [a] -> Int -> Int
+bound l n =
+  if n >= length l
+    then length l - 1
+    else n
+
+-- | Finding the index of the array given the time step and the accumulated
+--   time.
+index :: Float -> Float -> Int
+index little big = floor $ big / little
+
+-- | Accumulating time.
+accumTime :: Signal Bool -> SignalGen Float (Signal Float)
+accumTime sreset =
+  transfer 0 accumTime' sreset
+  where accumTime' :: Float -> Bool -> Float -> Float
+        accumTime'  _ True _ = 0
+        accumTime' dt    _ t = dt + t
+
+-- | Creating a timer that loops between 0 and a given value.
+accumTimeLoop :: Float -> SignalGen Float (Signal Float)
+accumTimeLoop cap =
+  mfix accumTimeLoop'
+  where accumTimeLoop' :: Signal Float -> SignalGen Float (Signal Float)
+        accumTimeLoop' t = do
+          at <- accumTime $ (>) <$> t <*> pure cap
+          delay 0 $ at
+
+-- | Creating a timer that NEVER resets.
+accumTimeNoLoop :: SignalGen Float (Signal Float)
+accumTimeNoLoop = accumTime $ pure False
+
+-- | The back end to stepping the array, given a @'SignalGen' 'Float' ('Signal' 'Float')@
+stepArray' :: SignalGen Float (Signal Float) -> [a] -> Float -> SignalGen Float (Signal a)
+stepArray' tgen list step =
+  liftA (\t -> (!!) <$> pure list <*> liftA (bound list . index step) t) tgen
+
+-- | Stepping through a list at a given rate (with the option of looping).
+stepArray :: Bool -> [a] -> Float -> SignalGen Float (Signal a)
+stepArray False list step = stepArray' (accumTimeNoLoop                                  ) list step
+stepArray  True list step = stepArray' (accumTimeLoop (fromIntegral (length list) * step)) list step
