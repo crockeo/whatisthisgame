7b99b4f10d19484b84d006f5728ed4693018f076
Crockeo <crockeo@gmail.com>
2014-10-06 16:38:36 -0400

    Fixed a BUNCH of stuff with the animation.

--- src/WhatIsThisGame/Animation.hs
+++ src/WhatIsThisGame/Animation.hs
@@ -37,35 +37,33 @@ index little big = floor $ big / little
 -- | Accumulating time.
 accumTime :: (HasTime t s, Monad m) => Float -> Wire s e m Bool Float
 accumTime it =
-  mkSF $ \ds r ->
-    if r
-      then (it, accumTime 0)
-      else
-        let dt = realToFrac $ dtime ds
-            t' = it + dt in
-          t' `seq` (it, accumTime t')
+  mkSF accumTime'
+  where accumTime' :: (HasTime t s, Monad m) => s -> Bool -> (Float, Wire s e m Bool Float)
+        accumTime' ds r =
+          if r
+            then (it, accumTime 0)
+            else
+              let dt = realToFrac $ dtime ds
+                  t' = it + dt in
+                t' `seq` (it, accumTime t')
 
 -- | Looping between 0 and a given point in time.
 accumTimeLoop :: (HasTime t s, MonadFix m) => Float -> Float -> Wire s e m a Float
 accumTimeLoop it mt =
   proc _ -> do
-    rec t <- accumTime it . mkSF_ (above mt) -< t
+    rec t <- accumTime it . delay False . mkSF_ (above mt) -< t
     returnA -< t
   where above :: Float -> Float -> Bool
         above cap n = n >= cap
 
 -- | Counting up infinitely from 0 (in real time).
 accumTimeNoLoop :: (HasTime t s, Monad m) => Float -> Wire s e m a Float
-accumTimeNoLoop it =
-  accumTime it . pure False
+accumTimeNoLoop it = accumTime it . pure False
 
 -- | The back-end of @'stepArray'@.
 stepArray' :: (HasTime t s, Monad m) => Wire s e m a Float -> [b] -> Float -> Wire s e m a b
 stepArray' timer l s =
-  proc _ -> do
-    t <- timer                              -< undefined
-    i <- mkSF_ ((!!) l . bound l . index s) -< t
-    returnA -< i
+  mkSF_ ((!!) l . bound l . index s) . timer
 
 -- | Stepping through an array in real-time.
 stepArray :: (HasTime t s, MonadFix m) => Bool -> [b] -> Float -> Wire s e m a b
@@ -82,41 +80,3 @@ animateTransform sloop animation step =
   mkSF_ makeTransform . animate sloop animation step
   where makeTransform :: String -> EntityTransform
         makeTransform name = \e -> e { getName = name }
-
-{-
-
--- | Creating a timer that loops between 0 and a given value.
-accumTimeLoop :: Float -> SignalGen Float (Signal Float)
-accumTimeLoop cap =
-  mfix accumTimeLoop'
-  where accumTimeLoop' :: Signal Float -> SignalGen Float (Signal Float)
-        accumTimeLoop' t = do
-          at <- accumTime $ (>) <$> t <*> pure cap
-          delay 0 $ at
-
--- | Creating a timer that NEVER resets.
-accumTimeNoLoop :: SignalGen Float (Signal Float)
-accumTimeNoLoop = accumTime $ pure False
-
--- | The back end to stepping the array, given a @'SignalGen' 'Float' ('Signal' 'Float')@
-stepArray' :: SignalGen Float (Signal Float) -> [a] -> Float -> SignalGen Float (Signal a)
-stepArray' tgen list step =
-  liftA (\t -> (!!) <$> pure list <*> liftA (bound list . index step) t) tgen
-
--- | Stepping through a list at a given rate (with the option of looping).
-stepArray :: Bool -> [a] -> Float -> SignalGen Float (Signal a)
-stepArray False list step = stepArray' (accumTimeNoLoop                                  ) list step
-stepArray  True list step = stepArray' (accumTimeLoop (fromIntegral (length list) * step)) list step
-
--- | Stepping through an animation at a given rate.
-animate :: Bool -> Animation -> Float -> SignalGen Float (Signal String)
-animate loop (Animation list) step = stepArray loop list step
-
--- | Creating an entity transform to set the sprite to the current sprite in
---   the animation.
-animateTransform :: Bool -> Animation -> Float -> SignalGen Float (Signal EntityTransform)
-animateTransform loop anim step = do
-  sframe <- animate loop anim step
-  return $ fmap (\frame -> \e -> e { getName = frame }) sframe
-
--}
