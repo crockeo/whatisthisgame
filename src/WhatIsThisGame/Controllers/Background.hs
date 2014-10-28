a718fc9f0a4c7d7725147b6cb3d1b8e61eb6e112
Crockeo <crockeo@gmail.com>
2014-10-28 17:16:01 -0400

    Asset update.

--- src/WhatIsThisGame/Controllers/Background.hs
+++ src/WhatIsThisGame/Controllers/Background.hs
@@ -22,16 +22,18 @@ initialBackground :: Entity
 initialBackground =
   Entity { getName     = "res/background.png"
          , getPosition = V2   0 0
-         , getSize     = V2   0 0
+         , getSize     = V2   100 100
          , getHealth   = 0
          , shouldShoot = False
          }
 
 -- | The controller that generates the transform each update.
 backgroundController :: Signal World -> SignalGen Float (Signal EntityTransform)
-backgroundController _ = do
+backgroundController _ = return $ return id
+{-
   ssize <- renderSize
   return $ liftM (\size -> \e -> e { getSize = size & _x .~ (size ^. _x * 100) }) ssize
+-}
 
 -- | The front-end for the background.
 background :: Signal World -> SignalGen Float (Signal Entity)
